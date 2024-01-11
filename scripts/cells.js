c3675628a461ab6f34bf44a146fea0d70df990e4
Kuba Sierpień <kubasierpien05@gmail.com>
2024-01-11 13:19:43 +0100

    typing changes

--- scripts/cells.js
+++ scripts/cells.js
@@ -7,3 +7,14 @@ for(let i = 0; i<25; i++){
 
     grid[0].appendChild(cell);
 }
+
+let cellCounter = 1;
+document.addEventListener("keydown", event => {
+    if(event.keyCode >=65 && event.keyCode <= 122){
+        grid[0].children[cellCounter].innerHTML = event.key;
+        cellCounter++;
+    }
+    if(event.keyCode === 9){
+        
+    }
+})
